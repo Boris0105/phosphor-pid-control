5bcfa79e0287bb93366f6ade908835317a54a36e
Boris Tsai - TW (LAB) <BorisT@supermicro.com>
2023-11-16 14:27:33 -0800

    add log function

--- log.cpp
+++ log.cpp
@@ -1,12 +1,9 @@
 #include "log.hpp"
 
-
 template <typename... Args>
 void log(const std::string& msg, Args... args) {
     std::ostringstream oss;
     oss << msg;
-
     ((oss << " " << args), ...);
-
     std::cout << oss.str() << std::endl;
 }
--- log.hpp
+++ log.hpp
@@ -3,6 +3,5 @@
 #include <iostream>
 #include <sstream> 
 
-
 template <typename... Args>
 void log(const std::string& msg, Args... args);
