6b52f71b6a4518fcaf78a7c4c2f4a2af720775af
Boris Tsai - TW (LAB) <BorisT@supermicro.com>
2023-11-16 14:18:25 -0800

    Add the log functions into PID module

---
+++ log.cpp
@@ -0,0 +1,12 @@
+#include "log.hpp"
+
+
+template <typename... Args>
+void log(const std::string& msg, Args... args) {
+    std::ostringstream oss;
+    oss << msg;
+
+    ((oss << " " << args), ...);
+
+    std::cout << oss.str() << std::endl;
+}
---
+++ log.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <sstream> 
+
+
+template <typename... Args>
+void log(const std::string& msg, Args... args);
